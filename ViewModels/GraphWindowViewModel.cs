7965a52445e3b35a4f2b822a8c3d6b9ffd9425c6
Koryy <iluwka13@gmail.com>
2023-10-26 01:20:35 +0700

    add show charts

--- ViewModels/GraphWindowViewModel.cs
+++ ViewModels/GraphWindowViewModel.cs
@@ -20,14 +20,12 @@ namespace LasAnalyzer.ViewModels
 {
     public class GraphWindowViewModel : ViewModelBase
     {
-        private PlotModel nearProbeModel;
-        private PlotModel farProbeModel;
-        private PlotModel farToNearProbeRatioModel;
-        private PlotModel temperatureModel;
-
         private DataGenerator dataGenerator;
 
-        public ISeries[] Series { get; set; }
+        public ISeries[] NearProbeSeries { get; set; }
+        public ISeries[] FarProbeSeries { get; set; }
+        public ISeries[] FarToNearProbeRatioSeries { get; set; }
+        public ISeries[] TemperatureSeries { get; set; }
 
         public LabelVisual Title { get; set; } =
         new LabelVisual
@@ -40,66 +38,46 @@ namespace LasAnalyzer.ViewModels
 
         public GraphWindowViewModel()
         {
-            InitializeGraphs();
+            dataGenerator = new DataGenerator();
 
-            Series = new ISeries[]
+            var newData = dataGenerator.GenerateGraphData(100);
+
+            NearProbeSeries = new ISeries[]
             {
                 new LineSeries<double>
                 {
-                    Values = dataGenerator.GenerateGraphData(100).NearProbe,
-                    Fill = null
+                    Values = newData.NearProbe,
                 }
             };
-        }
 
-        public PlotModel NearProbeModel
-        {
-            get => nearProbeModel;
-            set => this.RaiseAndSetIfChanged(ref nearProbeModel, value);
-        }
+            FarProbeSeries = new ISeries[]
+            {
+                new LineSeries<double>
+                {
+                    Values = newData.FarProbe,
+                }
+            };
 
-        public PlotModel FarProbeModel
-        {
-            get => farProbeModel;
-            set => this.RaiseAndSetIfChanged(ref farProbeModel, value);
-        }
+            FarToNearProbeRatioSeries = new ISeries[]
+            {
+                new LineSeries<double>
+                {
+                    Values = newData.FarToNearProbeRatio,
+                }
+            };
 
-        public PlotModel FarToNearProbeRatioModel
-        {
-            get => farToNearProbeRatioModel;
-            set => this.RaiseAndSetIfChanged(ref farToNearProbeRatioModel, value);
-        }
+            TemperatureSeries = new ISeries[]
+            {
+                new LineSeries<double>
+                {
+                    Values = newData.Temperature,
+                }
+            };
 
-        public PlotModel TemperatureModel
-        {
-            get => temperatureModel;
-            set => this.RaiseAndSetIfChanged(ref temperatureModel, value);
-        }
-
-        public ReactiveCommand<Unit, Unit> UpdateGraphs { get; set; }
-
-        private void InitializeGraphs()
-        {
-            dataGenerator = new DataGenerator();
-
-            // Инициализация моделей графиков и настройки по умолчанию
-            NearProbeModel = CreateGraphModel("Near Probe", "X-Axis Title", "Y-Axis Title");
-            FarProbeModel = CreateGraphModel("Far Probe", "X-Axis Title", "Y-Axis Title");
-            FarToNearProbeRatioModel = CreateGraphModel("Far/Near Ratio", "X-Axis Title", "Y-Axis Title");
-            TemperatureModel = CreateGraphModel("Temperature", "X-Axis Title", "Y-Axis Title");
-
-            // Привязка команды для обновления данных графиков
             UpdateGraphs = ReactiveCommand.CreateFromObservable(UpdateGraphData);
         }
 
-        private PlotModel CreateGraphModel(string title, string xTitle, string yTitle)
-        {
-            var model = new PlotModel { Title = title };
-            model.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom, Title = xTitle });
-            model.Axes.Add(new LinearAxis { Position = AxisPosition.Left, Title = yTitle });
-
-            return model;
-        }
+        public ReactiveCommand<Unit, Unit> UpdateGraphs { get; set; }
 
         private IObservable<Unit> UpdateGraphData()
         {
@@ -108,15 +86,15 @@ namespace LasAnalyzer.ViewModels
             var xData = newData.Time; // Ваши данные по оси X
             var yData = newData.NearProbe; // Ваши данные по оси Y
 
-            var newNearProbeModel = CreateGraphModel("Near Probe", "X-Axis Title", "Y-Axis Title");
-            var series = new LineSeries();
-            for (int i = 0; i < xData.Count; i++)
+            var updatedSeries = new ISeries[]
             {
-                series.Points.Add(new DataPoint(xData[i], yData[i]));
-            }
-            newNearProbeModel.Series.Add(series);
+                new LineSeries<double>
+                {
+                    Values = yData,
+                }
+            };
 
-            NearProbeModel = newNearProbeModel;
+            NearProbeSeries = updatedSeries;
 
             // Верните Unit для завершения команды
             return Observable.Return(Unit.Default);
